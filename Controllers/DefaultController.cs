f33cb1bfb43d79d77672403821ddc620629c282d
Ahmed Turan Sözcü <116086493+w4nnafly@users.noreply.github.com>
2023-07-03 17:29:26 +0300

    MVC5 ile Admin Panelli CV Sitesi Projesi
    MVC5 ile Admin Panelli CV Sitesi Projesi

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,62 @@
+﻿using MvcCv.Models.Entity;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+namespace MvcCv.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+
+            return View(degerler);
+        }
+        public PartialViewResult SosyalMedya()
+        {
+            var sosyalmedya = db.TblSosyalMedya.Where(x => x.Durum==true).ToList();
+            return PartialView(sosyalmedya);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Eğitimlerim()
+        {
+            var egitimler = db.TblEğitimlerim.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult Sertifikalarım()
+        {
+            var sertifikalar = db.TblSertifikalarım.ToList();
+            return PartialView(sertifikalar);
+        }
+        [HttpGet]
+        public PartialViewResult İletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult İletisim(Tblİletişim t)
+        {
+            t.Tarih=DateTime.Parse(DateTime.Now.ToString());
+            db.Tblİletişim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
---
+++ Controllers/EgitimController.cs
@@ -0,0 +1,69 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    [Authorize]
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<TblEğitimlerim> repo = new GenericRepository<TblEğitimlerim>();
+
+       
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(TblEğitimlerim p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimDüzenle(int id)
+        {
+            var egitim=repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+
+        [HttpPost]
+        public ActionResult EgitimDüzenle(TblEğitimlerim t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimDüzenle");
+            }
+            var egitim = repo.Find(x => x.ID == t.ID);
+            egitim.Baslik=t.Baslik;
+            egitim.AltBaslik1=t.AltBaslik1;
+            egitim.AltBaslik2 = t.AltBaslik2;
+            egitim.Tarih=t.Tarih;
+            egitim.GNO=t.GNO;
+            repo.TUpdate(egitim);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
